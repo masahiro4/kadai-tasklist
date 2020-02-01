09b77bc32cf3e14dbabf141666be91c5cfd10604
masahiro.yoshioka <ec2-user@ip-172-31-25-179.ec2.internal>
2020-02-01 12:44:13 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,16 +4,33 @@ class TasksController < ApplicationController
     end
     
     def show
-        @task = Task.find.(params[:id])
+        @task = Task.find(params[:id])
     end
     
     def new
         @task = Task.new
     end
     
+    def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = 'Tasak が正常に投稿されました'
+        else
+            flash.now[:dager] = 'Task が投稿されませんでした'
+            render :new
+        end
+    end
+    
     def edit
-        @task = Task.find.(params[:id])
+        @task = Task.find(params[:id])
     end
     
     
 end
+
+
+#Strong Parameter
+   def task_params
+       params.require(:task).permit(:content)
+   end
